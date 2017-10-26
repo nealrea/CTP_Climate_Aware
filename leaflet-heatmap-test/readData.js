b37d40d490b7f59d76dffe2b331e64d28a4a5381
Neal Rea <nealrea818@gmail.com>
2017-10-26 12:15:45 -0400

    add max field

--- leaflet-heatmap-test/readData.js
+++ leaflet-heatmap-test/readData.js
@@ -9,13 +9,24 @@ var reader = new NetCDFReader(data);
 var lat = reader.getDataVariable('lat');
 var lon = reader.getDataVariable('lon');
 var precip = reader.getDataVariable('prec');
-var prec = [];
+var prec = {
+	data: [],
+};
 
 var precCount = 0;
-for(var i = 0; i < lat.length; i++){
-	for(var j = 0; j < lon.length; j++){
-		prec.push([lat[i], lon[j], precip[precCount++]])
+for(var i in lat){
+	for(var j in lon){
+		prec.data.push({
+			lat: lat[i],
+			lon: lon[j],
+			value: precip[precCount++],
+		})
 	}
 }
 
+var max = prec.data.reduce((max, p) => p.value > max ? p.value : max, prec.data[0].value)
+prec.max = max;
+
 console.log(prec);
+
+module.exports = prec;
