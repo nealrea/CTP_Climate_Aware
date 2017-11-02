f729caf267294265c9af799731b1036b2bb0bd01
Neal Rea <nealrea818@gmail.com>
2017-11-02 14:17:12 -0400

    convert kelvin -> celsius, fix lat/lon dimensions

--- app/readData.js
+++ app/readData.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const NetCDFReader = require('netcdfjs')
 
-var file = 'ANN1960-1969.aijD0anl_0k.nc'; //local netCDF file
+var file = 'tas_Amon_GISS-E2-H-CC_rcp85_r1i1p1_205101-210012.nc'; //local netCDF file
 
 const data = fs.readFileSync(file);
 var reader = new NetCDFReader(data);
 
 var lat = reader.getDataVariable('lat');
 var lon = reader.getDataVariable('lon');
-var diagnostic = reader.getDataVariable('prec');
+var diagnostic = reader.getDataVariable('tas')[0];
 var dataObj = {
 	points: [],
 };
@@ -18,8 +18,8 @@ for(var i in lat){
 	for(var j in lon){
 		dataObj.points.push({
 			lat: lat[i],
-			lon: lon[j],
-			value: diagnostic[valueCount++],
+			lon: lon[j] - 180,
+			value: diagnostic[valueCount++] - 273.15,
 		})
 	}
 }
@@ -27,7 +27,7 @@ for(var i in lat){
 var max = dataObj.points.reduce((max, p) => p.value > max ? p.value : max, dataObj.points[0].value)
 dataObj.max = max;
 
-//console.log(prec);
+console.log(diagnostic[145]);
 
 fs.writeFile("./test.json", JSON.stringify(dataObj));
 
