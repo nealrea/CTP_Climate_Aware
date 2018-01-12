34947684a374cefddeeef490969e9e3e819064b3
Neal Rea <nealrea818@gmail.com>
2018-01-12 16:58:09 -0500

    revert PORT settings

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(viewHelpers.register());
 
 app.use(require('./controllers/'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 models.sequelize.sync().then(() => {
   app.listen(PORT, () => {
