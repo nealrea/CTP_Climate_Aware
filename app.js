2d67ea998909fc03031622e0127903363c24e089
Neal Rea <nealrea818@gmail.com>
2018-01-12 16:51:46 -0500

    edit PORT

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(viewHelpers.register());
 
 app.use(require('./controllers/'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 models.sequelize.sync().then(() => {
   app.listen(PORT, () => {
